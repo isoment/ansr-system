ab482f7991e55b46b3fdeebe7c01f1c498f0b91f
twill <twill1@gmx.com>
2020-12-16 13:44:12 -0600

    Add tests for TenantRequestIndex livewire component

--- tests/Feature/TenantRequestIndexTest.php
+++ tests/Feature/TenantRequestIndexTest.php
@@ -2,13 +2,17 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\TenantRequestIndex;
 use App\Models\Lease;
 use App\Models\Property;
 use App\Models\Region;
+use App\Models\RequestCategory;
+use App\Models\ServiceRequest;
 use App\Models\Tenant;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class TenantRequestIndexTest extends TestCase
@@ -18,7 +22,7 @@ class TenantRequestIndexTest extends TestCase
     /**
      *  @test
      * 
-     *  Make sure the livewire component is displayed.
+     *  The livewire component should display.
      */
     public function tenant_request_index_page_shows_livewire_component()
     {
@@ -28,7 +32,120 @@ class TenantRequestIndexTest extends TestCase
     }
 
     /**
-     *  Create a tenant for testing
+     *  @test
+     * 
+     *  The tenant should see only their requests
+     */
+    public function tenant_should_only_see_their_service_requests()
+    {
+        $tenantOne = $this->createTestingTenant();
+
+        $tenantTwo = $this->createTestingTenant();
+
+        $requestTennantOne = ServiceRequest::factory()->create([
+            'tenant_id' => $tenantOne->userable->id,
+            'category_id' => RequestCategory::factory()->create()->id,
+            'issue' => 'x*&hj3jdiosf983',
+            'completed_date' => NULL,
+        ]);
+
+        $requestTennantTwo = ServiceRequest::factory()->create([
+            'tenant_id' => $tenantTwo->userable->id,
+            'category_id' => RequestCategory::factory()->create()->id,
+            'issue' => '6@E87djdjiaf%b',
+            'completed_date' => NULL,
+        ]);
+
+        $this->actingAs($tenantOne);
+
+        Livewire::test(TenantRequestIndex::class)
+            ->assertSee($requestTennantOne->issue)
+            ->assertDontSee($requestTennantTwo->issue);
+    }
+
+    /**
+     *  @test
+     * 
+     *  The livewire component shows the tenants service requests
+     */
+    public function tenant_service_requests_are_shown()
+    {
+        $tenant = $this->createTestingTenant();
+
+        $this->actingAs($tenant);
+
+        $requestOne = ServiceRequest::factory()->create([
+            'tenant_id' => $tenant->userable->id,
+            'category_id' => RequestCategory::factory()->create()->id,
+            'issue' => 'Pipes',
+            'completed_date' => NULL,
+        ]);
+
+        $requestTwo = ServiceRequest::factory()->create([
+            'tenant_id' => $tenant->userable->id,
+            'category_id' => RequestCategory::factory()->create()->id,
+            'issue' => 'Fridge',
+        ]);
+
+        Livewire::test(TenantRequestIndex::class)
+            ->assertSee($requestOne->issue)
+            ->assertDontSee($requestTwo->issue)
+            ->set('open', false)
+            ->assertSee($requestTwo->issue)
+            ->assertDontSee($requestOne->issue);
+    }
+
+    /**
+     *  @test
+     * 
+     *  The requests can be sorted in asc or desc when column is clicked
+     */
+    public function request_are_displayed_in_asc_or_desc_order_by_created_date()
+    {
+        $tenant = $this->createTestingTenant();
+
+        $this->actingAs($tenant);
+
+        $requestOne = ServiceRequest::factory()->create([
+            'tenant_id' => $tenant->userable->id,
+            'category_id' => RequestCategory::factory()->create()->id,
+            'issue' => 'anterior-91',
+            'completed_date' => NULL,
+            'created_at' => '2020-12-10 21:19:32'
+        ]);
+
+        $requestTwo = ServiceRequest::factory()->create([
+            'tenant_id' => $tenant->userable->id,
+            'category_id' => RequestCategory::factory()->create()->id,
+            'issue' => 'suspicious-772',
+            'completed_date' => NULL,
+            'created_at' => '2020-12-12 09:12:22'
+        ]);
+
+        $requestThree = ServiceRequest::factory()->create([
+            'tenant_id' => $tenant->userable->id,
+            'category_id' => RequestCategory::factory()->create()->id,
+            'issue' => 'structure-125',
+            'completed_date' => NULL,
+            'created_at' => '2020-12-14 11:07:02'
+        ]);
+
+        Livewire::test(TenantRequestIndex::class)
+            ->assertSeeInOrder([
+                $requestThree->issue,
+                $requestTwo->issue,
+                $requestOne->issue
+            ])
+            ->call('sortDirection')
+            ->assertSeeInOrder([
+                $requestOne->issue,
+                $requestTwo->issue,
+                $requestThree->issue
+            ]);
+    }
+
+    /**
+     *  Create a tenant User for testing.
      */
     public function createTestingTenant()
     {
@@ -49,9 +166,12 @@ class TenantRequestIndexTest extends TestCase
             'lease_id' => $lease->id
         ]);
 
-        return User::factory()->create([
+        return User::create([
+            'name' => 'TestTenant',
+            'email' => $tenant->email,
             'userable_type' => 'App\Models\Tenant',
-            'userable_id' => $tenant->id
+            'userable_id' => $tenant->id,
+            'password' => bcrypt('password'),
         ]);
     }
 }
