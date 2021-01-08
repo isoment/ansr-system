46875b6f436ec90e875caaca75778fddaf2de0b0
twill <twill1@gmx.com>
2021-01-07 18:25:43 -0600

    Add form on the work order management page

--- app/Http/Livewire/EmployeeWorkOrderManage.php
+++ app/Http/Livewire/EmployeeWorkOrderManage.php
@@ -2,14 +2,58 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Employee;
+use App\Models\WorkOrder;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class EmployeeWorkOrderManage extends Component
 {
     public $workOrder;
 
+    public $assignment;
+    public $startdate;
+    public $enddate;
+
+    protected $rules = [
+        'assignment' => 'string|required',
+        'startdate' => 'date',
+        'enddate' => 'date',
+    ];
+
+    public function mount($workOrder)
+    {
+        $this->assignment = $workOrder->employee->employee_id_number;
+        $this->startdate = $this->mysqlToViewDateConversion($workOrder->start_date);
+        $this->enddate = $this->mysqlToViewDateConversion($workOrder->end_date);
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submitForm($workOrderId)
+    {
+        $this->validate();
+
+        $workOrder = WorkOrder::find($workOrderId);
+    }
+
+    /**
+     *  Method to format the date from the database into a format for the form
+     */
+    private function mysqlToViewDateConversion($date) 
+    {
+        return Carbon::parse($date)->toDateString();
+    }
+
     public function render()
     {
-        return view('livewire.employee-work-order-manage');
+        return view('livewire.employee-work-order-manage', [
+
+            'employees' => Employee::orderBy('last_name', 'asc')->get(),
+
+        ]);
     }
 }
--- resources/views/livewire/employee-work-order-manage.blade.php
+++ resources/views/livewire/employee-work-order-manage.blade.php
@@ -4,48 +4,82 @@
     <div class="break-words lg:col-span-2 bg-white text-gray-700 sm:border-1 rounded-sm sm:rounded-md mb-4 py-6 px-4 md:px-12 shadow-sm">
         <h5 class="text-xl font-bold text-center">Manage Work Order</h5>
 
-        <div class="px-12">
-            {{-- Assigned --}}
-            <div class="w-full mt-4 sm:mt-8">
-                <div class="flex justify-between items-center w-full">
-                    <label for="assignment" class="text-xs text-gray-700 font-bold">Assigned To:</label>
-                    @error('assignment')
-                        <div class="text-orange-400 text-xs font-bold italic">
-                            {{ $message }}
-                        </div>
-                    @enderror
+        <form wire:submit.prevent="submitForm({{$workOrder->id}})">
+            <div class="px-12">
+                {{-- Assigned --}}
+                <div class="w-full mt-4 sm:mt-8">
+                    <div class="flex justify-between items-center w-full">
+                        <label for="assignment" class="text-xs text-gray-700 font-bold">Assigned To:</label>
+                        @error('assignment')
+                            <div class="text-orange-400 text-xs font-bold italic">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="flex items-center mt-1">
+                        <input list="employees" name="assignment" id="assignment"
+                                class="px-4 w-full border bg-white rounded py-2 text-gray-700 focus:outline-none
+                                    @error('assignment') border-orange-400 @enderror" 
+                                value="{{$assignment}}"
+                                wire:model.debounce.500ms="assignment">
+                        <datalist id="employees">
+                            @foreach ($employees as $employee)
+                                <option value="{{$employee->employee_id_number}}">
+                                    {{$employee->last_name}}, {{$employee->first_name}}</option>
+                            @endforeach
+                        </datalist>
+                    </div>
                 </div>
-                <div class="flex items-center mt-1">
-                    <input type="text" id="assignment" name="assignment" placeholder="Assigned to"
-                        class="px-4 w-full border focus:border-teal-400 rounded py-2 text-gray-700 focus:outline-none
-                            @error('assignment') border-orange-400 @enderror" value="{{ old('assignment') }}" required 
-                        wire:model.debounce.500ms="assignment"/>
+                {{-- Start Date --}}
+                <div class="w-full mt-4 sm:mt-8">
+                    <div class="flex justify-between items-center w-full">
+                        <label for="startdate" class="text-xs text-gray-700 font-bold">Start Date:</label>
+                        @error('startdate')
+                            <div class="text-orange-400 text-xs font-bold italic">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="flex items-center mt-1">
+                        <input type="date" id="startdate" name="startdate"
+                            class="px-4 w-full border bg-white rounded py-2 text-gray-700 focus:outline-none
+                                @error('startdate') border-orange-400 @enderror" value="{{ old('startdate') }}" 
+                            wire:model.debounce.500ms="startdate"/>
+                    </div>
+                </div>
+                {{-- End Date --}}
+                <div class="w-full mt-4 sm:mt-8">
+                    <div class="flex justify-between items-center w-full">
+                        <label for="enddate" class="text-xs text-gray-700 font-bold">End Date:</label>
+                        @error('enddate')
+                            <div class="text-orange-400 text-xs font-bold italic">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="flex items-center mt-1">
+                        <input type="date" id="enddate" name="enddate" placeholder="Assigned to"
+                            class="px-4 w-full border bg-white rounded py-2 text-gray-700 focus:outline-none
+                                @error('enddate') border-orange-400 @enderror" value="{{ old('enddate') }}" 
+                            wire:model.debounce.500ms="enddate"/>
+                    </div>
+                </div>
+                <div class="mt-4 text-center">
+                    <button class="bg-teal-300 py-2 px-4 rounded-md text-white hover:bg-teal-400
+                                transition duration-200"
+                            type="submit">
+                        Submit
+                    </button>
                 </div>
             </div>
-            {{-- Start Date --}}
-            <div class="w-full mt-4 sm:mt-8">
-                <div class="flex justify-between items-center w-full">
-                    <label for="startdate" class="text-xs text-gray-700 font-bold">Start Date:</label>
-                    @error('startdate')
-                        <div class="text-orange-400 text-xs font-bold italic">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-                <div class="flex items-center mt-1">
-                    <input type="date" id="startdate" name="startdate" placeholder="Assigned to"
-                        class="px-4 w-full border bg-white rounded py-2 text-gray-700 focus:outline-none
-                            @error('startdate') border-orange-400 @enderror" value="{{ old('startdate') }}" required 
-                        wire:model.debounce.500ms="startdate"/>
-                </div>
-            </div>
-        </div>
+        </form>
+        
     </div>
 
     {{-- Col 2 --}}
     <div class="break-words bg-white text-gray-700 sm:border-1 rounded-sm sm:rounded-md mb-4 py-3 px-4 
-               shadow-sm flex flex-col justify-between">
-        <h5 class="font-bold text-center mb-4">Work Order Details</h5>
+               shadow-sm flex flex-col">
+        <h5 class="font-bold text-center mb-6">Work Order Details</h5>
         <div>
             <div class="my-2">
                 <span class="font-bold text-sm">Work Order ID:</span> 
