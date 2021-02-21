e05d9d02254f31b3b26a5a74d0346b32f61e4c4a
twill <twill1@gmx.com>
2021-02-21 15:03:46 -0600

    Finish layout of property listing show page

--- app/Http/Livewire/PropertyListingShow.php
+++ app/Http/Livewire/PropertyListingShow.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\PropertyListing;
 use Livewire\Component;
 
 class PropertyListingShow extends Component
@@ -10,6 +11,12 @@ class PropertyListingShow extends Component
 
     public function render()
     {
-        return view('livewire.property-listing-show');
+        return view('livewire.property-listing-show', [
+
+            'relatedRentals' => PropertyListing::whereHas('property.region', function($query) {
+                $query->where('region_name', $this->propertyListing->property->region->region_name);
+            })->inRandomOrder()->take(4)->get(),
+
+        ]);
     }
 }
--- resources/views/livewire/property-listing-show.blade.php
+++ resources/views/livewire/property-listing-show.blade.php
@@ -17,8 +17,50 @@
         <div class="col-span-2 p-4 bg-white shadow-md rounded-md">
             <img src="https://www.photoartistweb.nl/wp-content/uploads/2014/09/Real-Estate-Interior-030.jpg">
         </div>
-        <div class="col-span-1 mt-4 lg:mt-0 p-4 text-center bg-white shadow-md rounded-md">
-            Photo Thumbnails
+        <div class="col-span-1 mt-4 lg:mt-0 p-4 text-center bg-white shadow-md rounded-md
+                    grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-2 gap-4 overflow-y-scroll max-height-property-images">
+
+            <div class="rounded-md">
+                <img src="https://si.wsj.net/public/resources/images/MN-AM523_WHITE_P_20160718171125.jpg"
+                     class="rounded-md">
+            </div>
+            <div class="rounded-md">
+                <img src="https://www.digitalphotomentor.com/photography/2018/09/real-estate-living-room-photo-2.jpg"
+                     class="rounded-md">
+            </div>
+            <div class="rounded-md">
+                <img src="https://si.wsj.net/public/resources/images/MN-AM523_WHITE_P_20160718171125.jpg"
+                     class="rounded-md">
+            </div>
+            <div class="rounded-md">
+                <img src="https://www.digitalphotomentor.com/photography/2018/09/real-estate-living-room-photo-2.jpg"
+                     class="rounded-md">
+            </div>
+            <div class="rounded-md">
+                <img src="https://si.wsj.net/public/resources/images/MN-AM523_WHITE_P_20160718171125.jpg"
+                     class="rounded-md">
+            </div>
+            <div class="rounded-md">
+                <img src="https://www.digitalphotomentor.com/photography/2018/09/real-estate-living-room-photo-2.jpg"
+                     class="rounded-md">
+            </div>
+            <div class="rounded-md">
+                <img src="https://si.wsj.net/public/resources/images/MN-AM523_WHITE_P_20160718171125.jpg"
+                     class="rounded-md">
+            </div>
+            <div class="rounded-md">
+                <img src="https://www.digitalphotomentor.com/photography/2018/09/real-estate-living-room-photo-2.jpg"
+                     class="rounded-md">
+            </div>
+            <div class="rounded-md">
+                <img src="https://si.wsj.net/public/resources/images/MN-AM523_WHITE_P_20160718171125.jpg"
+                     class="rounded-md">
+            </div>
+            <div class="rounded-md">
+                <img src="https://www.digitalphotomentor.com/photography/2018/09/real-estate-living-room-photo-2.jpg"
+                     class="rounded-md">
+            </div>
+
         </div>
     </div>
 
@@ -146,6 +188,59 @@
             <button class="w-full bg-orange-500 text-white rounded-lg py-2 mt-3">
                 Request Info
             </button>
+        </div>
+    </div>
+
+    {{-- Related Rentals --}}
+    <div class="py-8">
+        <h3 class="text-lg font-bold">Similar Rentals You May Like</h3>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+            @foreach ($relatedRentals as $property)
+            {{-- Property Card --}}
+            <a href="{{route('property-listing-show', $property->id)}}">
+                {{-- Background Image --}}
+                <div class="h-48 rounded-lg relative"
+                    style="background-image: url('https://www.trulia.com/pictures/thumbs_3/zillowstatic/fp/23e98bb7154ed7532d6a1cc925b598a6-full.webp');
+                        background-size: cover;
+                        background-position: 50% 50%;">
+                    <div class="bg-white text-xs font-bold px-1 py-1 absolute top-2 left-2 rounded-lg">NEW</div>
+                </div>
+                {{-- Details --}}
+                <div class="mt-3">
+                    <div class="flex items-center justify-between">
+                        <h4 class="font-bold text-lg tracking-wider">${{$property->rent}}/mo</h4>
+                        <h6 class="text-sm capitalize">{{$property->type}}</h6>
+                    </div>
+                    <div class="flex mt-1">
+                        <div class="flex items-center mr-2">
+                            <svg class="svg h-5 w-5" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.196 14.603h15.523v.027h1.995v10.64h-3.99v-4.017H9.196v4.017h-3.99V6.65h3.99v7.953zm2.109-1.968v-2.66h4.655v2.66h-4.655z" fill="#869099"></path>
+                            </svg>
+                            <span class="font-light">{{$property->bedrooms}}bd</span>
+                        </div>
+                        <div class="flex items-center mr-2">
+                            <svg class="svg h-5 w-5" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M23.981 15.947H26.6v1.33a9.31 9.31 0 0 1-9.31 9.31h-2.66a9.31 9.31 0 0 1-9.31-9.31v-1.33h16.001V9.995a2.015 2.015 0 0 0-2.016-2.015h-.67c-.61 0-1.126.407-1.29.965a2.698 2.698 0 0 1 1.356 2.342H13.3a2.7 2.7 0 0 1 1.347-2.337 4.006 4.006 0 0 1 3.989-3.63h.67a4.675 4.675 0 0 1 4.675 4.675v5.952z" fill="#869099"></path>
+                            </svg>
+                            <span class="font-light">{{$property->bathrooms}}ba</span>
+                        </div>
+                        <div class="flex items-center">
+                            <svg class="svg h-5 w-5" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M13.748 21.276l-3.093-3.097v3.097h3.093zm12.852 5.32H10.655v.004h-5.32v-.004H5.32v-5.32h.015V5.32L26.6 26.596z" fill="#869099"></path>
+                            </svg>
+                            <span class="font-light">{{$property->sqft}} sqft</span>
+                        </div>
+                    </div>
+                    <div class="mt-1">
+                        <div class="mb-1">{{$property->property->street}}</div>
+                        @if ($property->unit)
+                            <div class="mb-1">Unit {{$property->unit}}</div>
+                        @endif
+                        <div><span>{{$property->property->city}}, </span><span>{{$property->property->state}}</span></div>
+                    </div>
+                </div>
+            </a>
+            @endforeach
         </div>
     </div>
 
