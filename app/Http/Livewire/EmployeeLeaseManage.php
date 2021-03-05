e40711f34a44b5a751ad5b86dcdaeeb00c9666ff
twill <twill1@gmx.com>
2021-03-04 22:06:08 -0600

    Validate the property being set on the lease create modal

--- app/Http/Livewire/EmployeeLeaseManage.php
+++ app/Http/Livewire/EmployeeLeaseManage.php
@@ -40,7 +40,25 @@ class EmployeeLeaseManage extends Component
      */
     public function setProperty($property)
     {
-        $this->selectedProperty = $property;
+        // Get an array of property ids based on role
+        if (Gate::allows('isManagement')) {
+
+            $propertyIds = Property::pluck('id')->toArray();
+
+        } else {
+
+            $propertyIds = Property::whereHas('region', function($query) {
+                $query->where('region_name', users_region());
+            })->pluck('id')->toArray();
+
+        }
+
+        // Check to make sure the property that is passed in is valid
+        if (in_array($property['id'], $propertyIds)) {
+
+            $this->selectedProperty = $property;
+
+        }
     }
 
     /**
