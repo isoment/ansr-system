9236e73e6cb974664e9f2efb61d3a76e0a594728
twill <twill1@gmx.com>
2021-03-08 01:48:16 -0600

    Change table for service request index to be more mobile friendly

--- resources/views/livewire/employee-service-request-index.blade.php
+++ resources/views/livewire/employee-service-request-index.blade.php
@@ -33,93 +33,54 @@
         </div>
     </div>
 
-
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg mt-4">
-                <table class="min-w-full divide-y divide-gray-200">
-                    <thead>
-                        <tr>
-                            <th class="px-6 py-3 bg-gray-50 text-left">
-                                <div class="flex items-center">
-                                    <div class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Issue</div>
-                                </div>
-                            </th>
-                            <th class="px-6 py-3 bg-gray-50 text-left">
-                                <div class="flex items-center">
-                                    <div class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</div>
-                                </div>
-                            </th>
-                            <th class="px-6 py-3 bg-gray-50 text-left">
-                                <div class="flex items-center">
-                                    <div class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Lease-ID</div>
-                                </div>
-                            </th>
-                            <th class="px-6 py-3 bg-gray-50 text-left">
-                                <div class="flex items-center">
-                                    <div class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Date</div>
-                                </div>
-                            </th>
-                            <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Status
-                            </th>
-                            <th class="px-6 py-3 bg-gray-50"></th>
-                        </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach ($requests as $request)
-                            <tr>
-                                <td class="w-4/12 px-6 py-4 whitespace-no-wrap">
-                                    <div class="flex items-center">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">
-                                                {{Str::limit($request->issue, 20)}}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="w-4/12 px-6 py-4 whitespace-no-wrap">
-                                    <div class="flex items-center">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">
-                                                {{$request->tenant->last_name}}, {{$request->tenant->first_name}}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="w-4/12 px-6 py-4 whitespace-no-wrap">
-                                    <div class="flex items-center">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">
-                                                {{$request->lease->id}}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="w-4/12 px-6 py-4 whitespace-no-wrap">
-                                    <div class="text-sm leading-5 text-gray-900">
-                                            {{\Carbon\Carbon::parse($request->created_at)->toFormattedDateString()}}
-                                    </div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-no-wrap">
-                                    @if ($request->completed_date)
-                                    <span
-                                        class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-green-100 text-green-400">
-                                        Complete
-                                    </span>
-                                    @else
-                                    <span
-                                        class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-teal-100 text-teal-400">
-                                        Open
-                                    </span>
-                                    @endif
-                                </td>
-                                <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
-                                    <a href="{{route('employee.manage-request', $request->id)}}"
-                                       class="text-teal-400 hover:text-teal-600 cursor-pointer">Details</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+    <div class="w-full overflow-hidden rounded-lg shadow-xs my-4">
+        <div class="w-full overflow-x-auto">
+            <table class="w-full whitespace-no-wrap">
+                <thead>
+                    <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase 
+                              border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                        <th class="px-4 py-3">Issue</th>
+                        <th class="px-4 py-3">Name</th>
+                        <th class="px-4 py-3">Date</th>
+                        <th class="px-4 py-3">Status</th>
+                        <th class="px-4 py-3"></th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+                    @foreach ($requests as $request)
+                        <tr class="text-gray-700 dark:text-gray-400">
+                            <td class="px-2 py-3 text-sm">
+                                {{Str::limit($request->issue, 20)}}
+                            </td>
+                            <td class="px-2 py-3 text-sm">
+                                {{$request->tenant->last_name}}, {{$request->tenant->first_name}}
+                            </td>
+                            <td class="px-2 py-3 text-sm">
+                                {{\Carbon\Carbon::parse($request->created_at)->toFormattedDateString()}}
+                            </td>
+                            <td class="px-2 py-3">
+                                @if ($request->completed_date)
+                                <span
+                                    class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-green-100 text-green-400">
+                                    Complete
+                                </span>
+                                @else
+                                <span
+                                    class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-teal-100 text-teal-400">
+                                    Open
+                                </span>
+                                @endif
+                            </td>
+                            <td class="px-2 py-3 text-sm">
+                                <a href="{{route('employee.manage-request', $request->id)}}"
+                                   class="text-teal-400 hover:text-teal-600 cursor-pointer">
+                                   Details
+                                </a>
+                            </td>
+                        </tr>  
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
