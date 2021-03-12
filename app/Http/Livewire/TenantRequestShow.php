324dc2579453a6cd735834ad36e9f00ae1bfe2eb
twill <twill1@gmx.com>
2021-03-12 00:01:56 -0600

    Finish tenant service request show page

--- app/Http/Livewire/TenantRequestShow.php
+++ app/Http/Livewire/TenantRequestShow.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\WorkDetails;
+use App\Models\WorkOrder;
 use Livewire\Component;
 
 class TenantRequestShow extends Component
 {
     public $serviceRequest;
+    public $selectedWorkOrder;
+
+    public function selectWorkOrder($workOrderId)
+    {
+        $this->selectedWorkOrder = $workOrderId;
+    }
+
+    public function workDetailCount($operator)
+    {
+        return WorkDetails::whereHas('workOrder.serviceRequest', function($query) {
+                $query->where('id', $this->serviceRequest->id);
+            })->where('end_date', $operator, null)->count();
+    }
 
     public function render()
     {
@@ -14,6 +29,15 @@ class TenantRequestShow extends Component
 
             'workOrders' => $this->serviceRequest->workOrders,
 
+            'currentWorkOrder' => WorkOrder::where('id', $this->selectedWorkOrder)
+                ->first(),
+
+            'workDetails' => WorkDetails::where('work_order_id', $this->selectedWorkOrder)
+                ->orderBy('created_at')->get(),
+
+            'workDetailsOpen' => $this->workDetailCount('='),
+
+            'workDetailsClosed' => $this->workDetailCount('!='),
         ]);
     }
 }
--- resources/views/livewire/tenant-request-show.blade.php
+++ resources/views/livewire/tenant-request-show.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 xl:grid-cols-3 gap-4">
         <div class="break-words bg-white text-gray-700 sm:border-1 rounded-sm sm:rounded-md mb-4 py-3 px-2 
                     shadow-sm flex flex-col justify-between">
             <div class="mx-2">
@@ -38,32 +38,90 @@
                     </div>
                 </div>
             </div>
+            <div class="text-center">
+                <div class="bg-white rounded shadow-md text-center px-8 py-6 mb-3 sm:mb-0 w-auto">
+                    <h4 class="font-bold text-lg font-prompt mb-2">Tenant Charges</h4>
+                    <h6 class="text-orange-300 font-bold font-prompt text-5xl">${{$serviceRequest->tenant_charges ? $serviceRequest->tenant_charges : 'N/A'}}</h6>
+                </div>
+            </div>
         </div>
-        <div class="break-words lg:col-span-2 bg-white text-gray-700 sm:border-1 rounded-sm 
+        <div class="break-words xl:col-span-2 bg-white text-gray-700 sm:border-1 rounded-sm 
                     sm:rounded-md mb-4 py-6 px-4 md:px-12 shadow-sm">
             <div>
                 <h3 class="font-bold font-prompt text-lg text-center">Activity</h3>
                 <div class="flex flex-col sm:flex-row justify-around items-center my-4">
                     <div class="bg-white rounded shadow-md text-center px-8 py-6 mb-3 sm:mb-0 w-2/3 sm:w-auto">
-                        <h3 class="text-teal-300 font-prompt font-bold text-3xl mb-4">2</h3>
+                        <h3 class="text-teal-300 font-prompt font-bold text-3xl mb-4">{{$workOrders->count()}}</h3>
                         <h6 class="text-uppercase font-thin text-sm">Work Orders</h6>
                     </div>
                     <div class="bg-white rounded shadow-md text-center px-8 py-6 mb-3 sm:mb-0 w-2/3 sm:w-auto">
-                        <h3 class="text-teal-300 font-prompt font-bold text-3xl mb-4">2</h3>
-                        <h6 class="text-uppercase font-thin text-sm">Total Tasks</h6>
+                        <h3 class="text-teal-300 font-prompt font-bold text-3xl mb-4">{{$workDetailsOpen}}</h3>
+                        <h6 class="text-uppercase font-thin text-sm">Tasks (Open)</h6>
+                    </div>
+                    <div class="bg-white rounded shadow-md text-center px-8 py-6 mb-3 sm:mb-0 w-2/3 sm:w-auto">
+                        <h3 class="text-teal-300 font-prompt font-bold text-3xl mb-4">{{$workDetailsClosed}}</h3>
+                        <h6 class="text-uppercase font-thin text-sm">Tasks (Ended)</h6>
                     </div>
                 </div>
             </div>
             <div class="mt-10">
-                <h3 class="font-bold font-prompt text-lg">Click Work Order for activity</h3>
-                <div class="flex flex-wrap items-center">
-                    @foreach ($workOrders as $workOrder)
-                        <div class="mr-2 my-2 p-2 border border-orange-300 rounded-md text-orange-300 text-sm
-                                    cursor-pointer">
-                            {{$workOrder->title}}
-                        </div>
-                    @endforeach
+                <div>
+                    <h3 class="font-bold font-prompt">Click Work Order for Activity</h3>
+                    <div class="flex flex-wrap items-center">
+                        @if ($workOrders->isNotEmpty())
+                            @foreach ($workOrders as $workOrder)
+                                <div class="mr-2 my-2 p-2 border border-teal-500 rounded-md text-teal-500 text-sm
+                                            cursor-pointer"
+                                    wire:click="selectWorkOrder({{$workOrder->id}})">
+                                    {{$workOrder->title}}
+                                </div>
+                            @endforeach
+                        @else
+                            <div class="my-2 font-thin">No Work Orders</div>
+                        @endif
+                    </div>
                 </div>
+                @if (!is_null($currentWorkOrder))
+                    <div class="my-8">
+                        <div class="flex justify-between items-center">
+                            <h4 class="font-bold">Work Order</h4>
+                            <div>
+                                @if ($currentWorkOrder->end_date)
+                                <div class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full 
+                                        bg-teal-100 text-teal-400">
+                                    Complete
+                                </div>
+                                @else
+                                <div class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs 
+                                            font-medium leading-4 bg-red-100 text-red-400">
+                                    Incomplete
+                                </div>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="mt-1">
+                            {{$currentWorkOrder->title}}
+                        </div>
+                        <div class="mt-4 overflow-y-auto max-h-80 pr-2">
+                            @foreach ($workDetails as $workDetail)
+                                <div class="border border-gray-400 rounded-lg p-2 my-2">
+                                    <div class="my-1">
+                                        <span class="font-bold">Activity:</span> 
+                                        <div class="text-sm font-light mt-1">
+                                            {{$workDetail->tenant_notes}}
+                                        </div>
+                                    </div>
+                                    <div class="my-1">
+                                        <span class="font-bold">Completed:</span> 
+                                        <div class="text-sm font-light mt-1">
+                                            {{$serviceRequest->end_date ? \Carbon\Carbon::parse($serviceRequest->end_date)->toFormattedDateString() : 'Not Completed'}}
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
